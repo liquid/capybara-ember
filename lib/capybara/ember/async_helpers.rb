7b29da353977c46d10ea06e9639bc71d85aff507
Uri Golani <urigolanigmail.com>
2017-06-02 11:34:17 -0700

    Fix deprecated default_wait_time to default_max_wait_time

--- lib/capybara/ember/async_helpers.rb
+++ lib/capybara/ember/async_helpers.rb
@@ -2,7 +2,7 @@ module Capybara
   module Ember
     module AsyncHelpers
       def wait_for_ember_run_loop_to_complete
-        Capybara.default_wait_time*100.times do #this means up to 5 seconds if Capybara.default_wait_time is 5
+        Capybara.default_max_wait_time*100.times do #this means up to 5 seconds if Capybara.default_wait_time is 5
           return if  Capybara.current_session.evaluate_script "(typeof Ember === 'object') && !Ember.run.hasScheduledTimers() && !Ember.run.currentRunLoop"
           sleep 0.01
         end
